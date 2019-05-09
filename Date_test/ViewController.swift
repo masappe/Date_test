060fd08e9f9e5df9e909e8e2d617c5490ffc2036
masappe <masato6820@gmail.com>
2019-05-09 12:45:01 +0900

    init

--- Date_test/ViewController.swift
+++ Date_test/ViewController.swift
@@ -13,6 +13,34 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let date = Date()
+        print("date\(date)")
+
+        let calendar = Calendar.current
+        var datecomponents = DateComponents()
+//        datecomponents.month = 2
+//        datecomponents.day = 1
+        datecomponents.hour = 9
+        //toの日付からbyAddingぶんずらす
+        let changeDate = calendar.date(byAdding: datecomponents, to: date)
+        print(changeDate)
+        
+        //日にちのフォーマット
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy/MM/dd"
+        let dateFormatter = formatter.string(from: date)
+        print(dateFormatter)
+        
+        //time zoneを使用
+        var components = Calendar.current.dateComponents([.year,.month,.day], from: date)
+        components.day = 1
+        let first = Calendar.current.date(from: components)
+        print("first\(first)")
+        
+        let component = calendar.dateComponents([.year,.month,.day], from: date
+            , to: first!)
+        let second = calendar.date(from: component)
+        print("second\(second)")
     }
 
 
